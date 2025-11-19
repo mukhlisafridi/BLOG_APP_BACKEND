6e946191b0d1505683b68cc0de8f0867699cabd0
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-11-19 11:15:12 +0500

    vercel deployement

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ import blogRoutes from "./routes/blog.routes.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://blog-app-frontend-teal.vercel.app'  
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const PORT = process.env.PORT || 3000;
 
