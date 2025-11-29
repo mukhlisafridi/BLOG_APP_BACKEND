b7e98ff3d722ca18c5952e7d1a7372ec061f34df
mukhlisafridi <mukhlisafridi4@gmail.com>
2025-11-29 22:54:13 +0500

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: [
-    'http://localhost:5173'  
+    'http://localhost:5173',"https://blog-app-frontend-rose.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
